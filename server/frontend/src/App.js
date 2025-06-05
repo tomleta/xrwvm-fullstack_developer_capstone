9669e6d1a8d2ff3a5b6e5f71c712aac3f9e4d99a
Tomleta <tholet26@gmail.com>
2025-06-05 18:11:50 +0200

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import LoginPanel from "./components/Login/Login"; // Importing the Login component
-import Register from "./components/Register/Register"; // Importing the Register components
+import { Routes, Route } from "react-router-dom";
+
+// Composants
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from './components/Dealers/Dealer';
 import PostReview from "./components/Dealers/PostReview";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-        <Route path="/login" element={<LoginPanel />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dealers" element={<Dealers/>} />
-        <Route path="/dealer/:id" element={<Dealer/>} />
-        <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
+      <Route path="*" element={<h1>404 - Page Not Found</h1>} />
     </Routes>
   );
 }
+
 export default App;
