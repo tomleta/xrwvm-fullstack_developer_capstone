967f34ff62c265855e692b0c4e678093ae2d3f5d
Tomleta <tholet26@gmail.com>
2025-06-05 17:18:16 +0200

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -14,17 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
+import os  # Import os for path operations
 
 urlpatterns = [
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+    path('', TemplateView.as_view(template_name="Home.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
-    path('', TemplateView.as_view(template_name="Home.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
