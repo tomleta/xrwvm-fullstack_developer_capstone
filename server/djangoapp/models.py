437d9a47b831259cc49a8cf2e147c0b1bbc8c3ae
Tomleta <tholet26@gmail.com>
2025-06-06 16:41:38 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,34 +1,41 @@
 from django.db import models
-# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
-
 class CarMake(models.Model):
+    """Represents a car manufacturer"""
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    name = models.CharField(max_length=100)
+    """Represents a specific car model"""
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2025),
             MinValueValidator(2015)
-        ])
-    # Other fields as needed
+        ]
+    )
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name
