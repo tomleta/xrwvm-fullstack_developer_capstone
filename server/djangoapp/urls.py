80f3f7e25c243cf2567fcaac060f5a07d35c00f5
Tomleta <tholet26@gmail.com>
2025-06-06 17:06:32 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,18 +4,28 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    path(route='register', view=views.registration, name='register'),
-    path(route='login', view=views.login_user, name='login'),
+    path('register', views.registration, name='register'),
+    path('login', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>',
-         view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>',
-         view=views.get_dealer_reviews, name='dealer_details'),
-
-    path(route='add_review', view=views.add_review, name='add_review'),
-
+    path('get_cars', views.get_cars, name='getcars'),
+    path('get_dealers', views.get_dealerships, name='get_dealers'),
+    path(
+        'get_dealers/<str:state>',
+        views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        'dealer/<int:dealer_id>',
+        views.get_dealer_details,
+        name='dealer_details'
+    ),
+    path(
+        'reviews/dealer/<int:dealer_id>',
+        views.get_dealer_reviews,
+        name='dealer_details'
+    ),
+    path('add_review', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
